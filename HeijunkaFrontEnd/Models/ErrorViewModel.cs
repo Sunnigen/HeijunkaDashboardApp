a0702e6ea2cb5e8aa09f8f6a5e3fede207bd6553
Sunnigen <kalvarez144000@gmail.com>
2022-02-14 00:41:22 -0800

    Revert "Added front end draft, rendering a scheduler a few buttons."
    This reverts commit e63bb79a8b4543522277e68db838767bbf0b7668.

--- HeijunkaFrontEnd/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HeijunkaFrontEnd.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
