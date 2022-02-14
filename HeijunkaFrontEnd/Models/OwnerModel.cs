e63bb79a8b4543522277e68db838767bbf0b7668
Sunnigen <kalvarez144000@gmail.com>
2022-02-14 00:04:26 -0800

    Added front end draft, rendering a scheduler a few buttons.

---
+++ HeijunkaFrontEnd/Models/OwnerModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HeijunkaTest.Models
+{
+    public class OwnerModel
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Color { get; set; }
+
+    }
+}
