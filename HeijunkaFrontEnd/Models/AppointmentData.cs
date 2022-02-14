cf8870672e12090c6155b4cf2d3eba3eb04e63bf
Sunnigen <kalvarez144000@gmail.com>
2022-02-14 00:46:30 -0800

    Added front end draft.

---
+++ HeijunkaFrontEnd/Models/AppointmentData.cs
@@ -0,0 +1,11 @@
+﻿namespace HeijunkaTest.Models
+{
+    public class AppointmentData
+    {
+        public int Id { get; set; }
+        public int OwnerId { get; set; }
+        public string Subject { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
---
+++ HeijunkaFrontEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HeijunkaFrontEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
