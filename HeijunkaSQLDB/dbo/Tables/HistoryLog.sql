5a779568c1a405887a18e2852d58e5de19e1f582
Sunnigen <kalvarez144000@gmail.com>
2022-05-16 11:48:02 -0700

    Delete HeijunkaSQLDB directory

--- HeijunkaSQLDB/dbo/Tables/HistoryLog.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[HistoryLog]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [ProcessId] INT NOT NULL, 
-    [ChangeLog] NVARCHAR(200) NULL, 
-    CONSTRAINT [FK_HistoryLog_Heijunka] FOREIGN KEY (ProcessId) REFERENCES Heijunka(Id)
-)
--- HeijunkaSQLDB/dbo/Tables/Users.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Users]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [UserName] NVARCHAR(50) NOT NULL, 
-    [Password] NVARCHAR(50) NOT NULL, 
-    [RoleLevel] INT NOT NULL DEFAULT 0, 
-    [LastLoggedIn] DATE NOT NULL
-)
